4b7b88e01f7d59993d7b6abbf1f8e9b49546e21f
Ben Marshall <benleem98@yahoo.com>
2024-08-21 16:22:03 -0400

    FINALLY fixing the carousel overflow

--- static/scripts/glide.js
+++ static/scripts/glide.js
@@ -4,6 +4,28 @@ var glide = new Glide(".glide", {
 	keyboard: true,
 	startAt: 0,
 	focusAt: "center",
-	perView: 1,
+	perView: 3,
+	peek: {
+		before: 0,
+		after: 0,
+	},
+	gap: 20,
+	breakpoints: {
+		1200: {
+			perView: 2,
+		},
+		992: {
+			perView: 1,
+		},
+	},
 });
 glide.mount();
+
+// function resize() {
+// 	const { innerWidth } = window;
+// 	nav = document.getElementById("nav-aside");
+// 	main = document.querySelector("main");
+// 	main.style.width = `${innerWidth - nav.offsetWidth - 100}px`;
+// }
+// resize();
+// window.addEventListener("resize", resize);
