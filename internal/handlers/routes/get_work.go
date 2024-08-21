1130aef141fac5cb48e1d1a4bdd03c0677a934ea
Ben Marshall <benleem98@yahoo.com>
2024-08-20 22:24:08 -0400

    working on works carousel

--- internal/handlers/routes/get_work.go
+++ internal/handlers/routes/get_work.go
@@ -15,7 +15,16 @@ func NewWorkHandler() *WorkHandler {
 }
 
 func (h *WorkHandler) Init(c echo.Context) error {
-	works := []string{"Love Together", "prattl", "Goggle Earth", "Chirp", "Landing Page"}
+	// works := []string{"Love Together", "prattl", "ShowBeam", "Goggle Earth", "Chirp", "CanvaCast"}
+	works := []pages.WorkInfo{
+		{Name: "Love Together", Tags: []string{"TypeScript", "NextJs", "Tailwind"}, Live: "https://www.love-together.com/", Code: ""},
+		{Name: "prattl", Tags: []string{"Go", "Python"}, Live: "", Code: "https://github.com/benleem/prattl"},
+		{Name: "ShowBeam (client)", Tags: []string{"TypeScript", "SvelteKit", "Tailwind"}, Live: "", Code: "https://github.com/benleem/show_beam_client"},
+		{Name: "ShowBeam (server)", Tags: []string{"Rust", "Actix Web", "MySQL", "OAuth 2.0"}, Live: "", Code: "https://github.com/benleem/show_beam_server"},
+		{Name: "Goggle Earth", Tags: []string{"JavaScript", "React", "CSS", "ThreeJs"}, Live: "https://goggle-earth.netlify.app/", Code: "https://github.com/benleem/threejs-test"},
+		{Name: "Chirp", Tags: []string{"JavaScript", "NextJs", "CSS Modules"}, Live: "https://chirp-social.vercel.app/", Code: "https://github.com/benleem/chirp"},
+		{Name: "CanvaCast", Tags: []string{"HTML", "JavaScript", "CSS"}, Live: "https://htmlpreview.github.io/?https://github.com/benleem/CanvaCast/blob/main/index.html", Code: "https://github.com/benleem/CanvaCast"},
+	}
 	page := pages.Work(works)
 	hxReq := c.Request().Header.Get("hx-request")
 	if hxReq != "" {
@@ -26,4 +35,5 @@ func (h *WorkHandler) Init(c echo.Context) error {
 	// 	http.Error(w, "Error rendering template", http.StatusInternalServerError)
 	// 	return
 	// }
+
 }
---
+++ static/scripts/glide.js
@@ -0,0 +1,9 @@
+var glide = new Glide(".glide", {
+	type: "carousel",
+	autoplay: 3000,
+	keyboard: true,
+	startAt: 0,
+	focusAt: "center",
+	perView: 1,
+});
+glide.mount();
